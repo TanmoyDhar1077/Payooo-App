3f0a417e8b793053869d7ec1d2c59a006f941308
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-02-26 20:20:44 +0600

    Updated

--- assets/js/addmoney.js
+++ assets/js/addmoney.js
@@ -2,6 +2,20 @@ document.getElementById('add-money-submit').addEventListener('click', function (
     event.preventDefault();
     const amount = document.getElementById('add-money-amount').value;
     const amountFloat = parseFloat(amount);
-    console.log(amountFloat);
+    // console.log(amountFloat);
+    const pinNumber = document.getElementById('add-money-pin').value;
+    const convertedPin = parseInt(pinNumber);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const mainBalanceFloat = parseFloat(mainBalance);
+    
+    if(convertedPin === 1234){
+        const newBalance = mainBalanceFloat + amountFloat;
+        document.getElementById('main-balance').innerText = newBalance;
+        
+        // alert('Money Added Successfully');
+    }
+    else{
+        alert('Invalid Pin Number');
+    }
     
 });
